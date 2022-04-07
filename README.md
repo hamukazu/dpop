da7d9936ea97d1f038d92528933f2d4e06a49071
kimikazu Kato <kimikazu@gmail.com>
2022-04-07 17:51:59 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # dpop
 踊るうんこのgif画像を生成します。
+
+dpop stands for Dancing Pile of Poo.
+
+## 使い方
+
+今のところMacでのみ動作します。OS依存の部分はフォントの指定のところだけだと思われます。
+
+Python3がインストールされていることを前提とします。
+
+まず、以下のコマンドを実行して必要なパッケージをインストールします。
+```
+> pip install -r requirements.tzt
+```
+
+以下を実行するとファイルdpop.gifが生成されます。
+```
+> python dpop.py
+```
+
+オプションの指定ができますが、オプションの詳細は以下を実行してください。
+```
+> python dpop.py --help
+```
+
---
+++ dpop.py
@@ -0,0 +1,62 @@
+from PIL import Image, ImageDraw, ImageFont
+import math
+import sys
+import argparse
+
+
+def run(size, radius, shrinking, outfile):
+
+    images = []
+    fontsize = 40
+    if sys.platform == "darwin":
+        font = ImageFont.truetype(
+            "/System/Library/Fonts/Apple Color Emoji.ttc", fontsize
+        )
+    else:
+        print("Unsupported platform")
+        sys.exit(1)
+
+    for i in range(72):
+        im = Image.new("RGB", (size, size))
+        draw = ImageDraw.Draw(im)
+        for j in range(6):
+            t = (i * 5 + j * 60) * math.pi / 180
+            if shrinking:
+                r = radius * (1 - abs(i - 36) / 72)
+            else:
+                r = radius
+            draw.text(
+                (
+                    int(size / 2 + r * math.cos(t) - fontsize / 2),
+                    int(size / 2 + r * math.sin(t) - fontsize / 2),
+                ),
+                text="💩",
+                font=font,
+                embedded_color=True,
+            )
+        images.append(im)
+
+    images[0].save(
+        outfile,
+        save_all=True,
+        append_images=images[1:],
+        optimize=True,
+        duration=80,
+        loop=0,
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--size", type=int, default=300)
+    parser.add_argument("--radius", type=int, default=100)
+    parser.add_argument("--shrinking", type=bool, default=False, help="shrinking mode")
+    parser.add_argument(
+        "-o", "--output", type=str, default="dpop.gif", help="output file"
+    )
+    args = parser.parse_args(sys.argv[1:])
+    run(args.size, args.radius, args.shrinking, args.output)
+
+
+if __name__ == "__main__":
+    main()
